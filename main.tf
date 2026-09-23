778f57c61975f393e74b7d82b690bc1580710175
kunal <kp4u2011@gmail.com>
2026-09-23 23:00:09 +0530

    added-tf-codes

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-dev-eastus-001"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-dev-eastus-001"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "snet-dev-eastus-001"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
